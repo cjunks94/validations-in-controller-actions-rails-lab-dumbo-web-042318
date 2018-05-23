2f087f1854617132375dc187cf6a1860170c79fa
cjunks94 <cjunks94@gmail.com>
2018-05-23 11:33:08 -0400

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+  # Add validations to Author such that...
+  #
+  #   The name cannot be blank
+  #   The e-mail is unique
+validates :name, presence: true
+validates :email, presence: true, uniqueness: true
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ActiveRecord::Base
+  # Add validations to Post such that...
+  #
+  #   The title cannot be blank
+  #   The category is either "Fiction" or "Non-Fiction"
+  #   The content is at least 100 characters long.
+validates :title, presence: true
+validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+validates :content, length: {minimum: 100}
+
+  # def category_valid
+  #   if self.category == "Fiction" || self.category == "Non-Fiction"
+  #   else
+  #     false
+  #   end
+  # end
 end
